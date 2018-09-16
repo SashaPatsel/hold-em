31b106a0c1ccd4858905185e5109f7e19ea18891
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-09-16 11:45:34 -0700

    create game class

--- src/features/Game.js
+++ src/features/Game.js
@@ -1,9 +1,38 @@
 import React, {Component} from "react";
+import deck from "../data/deck.json"
 
 class Game extends Component {
-state = {
+  state = {
+    deck: deck
+  }
 
+  componentDidMount() {
+    console.log(this.state.deck)
+  }
+
+  //Knuth Shuffle
+  shuffle = (arr) => {
+  let currentIndex = arr.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = arr[currentIndex];
+    arr[currentIndex] = arr[randomIndex];
+    arr[randomIndex] = temporaryValue;
+  }
+
+  this.setState({
+    cards: arr
+  });
 }
+
+  
 
   render () {
     return (
