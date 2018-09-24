872089fa5c5ac94b820fae970004319414ba69a1
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-09-23 17:12:49 -0700

    finish create table form

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -6,38 +6,56 @@ import Input from "../components/Form/Input"
 class Create extends Component {
 
   state ={
-    bb: 0
+    tableName: "",
+    maxPlayers: 0,
+    bb: 0,
+    sb: 0,
+    buyBack: false,
+    public: false,
+    minBuyIn: 0,
+    maxBuyIn: 0
   }
 
   handleChange = e => {
+   const {name, value} = e.target
     this.setState({
-      bb: e.target.value
+      [name]: value,
+      minBuyIn: this.state.bb * 40,
+      maxBuyIn: this.state.bb * 100
     })
-  
+
   }
+
+  submitForm = e => {
+    e.preventDefault()
+    console.log(this.state)
+  }  
 
   render() {
     return (
       <div>
-        {this.state.bb}
-        <form>
-          <Input label="Table Name" type="text" />
-          <Input label="Max Players" type="number" max={9} />
-          <Input label="Big Blind" type="number" onChange={this.handleChange}/>
-          <Input label="Min Buy-In" type="number" />
-          <Input label="Max Buy-In" type="number" />
+        Min Buy-In: {this.state.minBuyIn}
+        <br/>
+        Max Buy-In: {this.state.maxBuyIn}
+
+
+        <form onSubmit={this.submitForm} >
+          <Input name="tableName" label="Table Name" type="text" onChange={this.handleChange}/>
+          <Input name="maxPlayers" label="Max Players" type="number" max={9} onChange={this.handleChange}/>
+          <Input name="bb" label="Big Blind" type="number"  step={.05} min={.1} onChange={this.handleChange}/>
+          <Input name="sb" label="Small Blind" type="number"  step={.05} min={.1} max={this.state.bb/2} onChange={this.handleChange}/>
           <label>Buy Backs</label>
-          <select>
-            <option>Yes</option>
-            <option>No</option>
+          <select name="buyBack" onChange={this.handleChange}>
+            <option value={true}>Yes</option>
+            <option  value={false}>No</option>
           </select>
           <label>Private/Public</label>
-          <select>
-            <option>Private</option>
-            <option>Public</option>
+          <select name="public" onChange={this.handleChange}>
+            <option value={false}>Private</option>
+            <option value={true}>Public</option>
           </select>
-          <Input />
-        </form>
+          <Input type="submit" value="Create Table"/>
+        </form >
       </div>
     )
   }
